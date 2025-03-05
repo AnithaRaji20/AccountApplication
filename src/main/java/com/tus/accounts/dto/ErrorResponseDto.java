be40ba11d6d987f1243daee358385c7f7639a110
AnithaRaji20 <a00325739@student.tus.ie>
2025-03-05 23:19:47 +0000

    Spring Account Application

---
+++ src/main/java/com/tus/accounts/dto/ErrorResponseDto.java
@@ -0,0 +1,13 @@
+package com.tus.accounts.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponseDto {
+    private String apiPath;
+    private String errorCode;
+    private String errorMessage;
+    private String errorTime;
+}
